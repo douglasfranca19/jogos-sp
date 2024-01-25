acb7a43fc3e4056170acc3bb0828403d9b5f9903
douglasfranca19 <francadouglas19@gmail.com>
2024-01-25 00:43:46 -0300

    Projeto Criado

---
+++ src/main/java/com/doufsp/jogossp/JogosspApplication.java
@@ -0,0 +1,13 @@
+package com.doufsp.jogossp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogosspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogosspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doufsp/jogossp/JogosspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doufsp.jogossp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogosspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
